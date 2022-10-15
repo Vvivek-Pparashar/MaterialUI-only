076eafa31e277e3eb4e2905b4e4fdd69d022369c
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2022-10-16 03:01:49 +0530

    updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import './Home.css'
-
+import { Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
     <>
-        <div className='home__hero'>
-            <div className='home__hero__img'></div>
-            
-        </div>
+      <div className="home__hero">
+        <div className="home__hero__img"></div>
+        <Container sx={{display:"flex", flexDirection: 'column', width : "50%" }}>
+          <Typography variant="h6">Huge Collection</Typography>
+          <Typography variant="h2">New Bags</Typography>
+          <Typography variant="p">
+            Torem ipsum dolor sit amet, consectetur adipisicing elitsed do
+            eiusmo tempor incididunt ut labore et dolore magna
+          </Typography>
+        </Container>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
