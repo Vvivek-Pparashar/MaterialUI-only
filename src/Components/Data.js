f133ddb1b61b029c62a2818b6b00a94b4bdb77d2
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2022-10-17 00:02:49 +0530

    images path changed

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -4,42 +4,42 @@ const products = [
         name: "Super Backpack",
         price: 129.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/bag_01.png"
+        image: "../images/products/bag_01.png"
     },
     {
         id: 2,
         name: "New Hip",
         price: 199.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/bag_02.png"
+        image: "../images/products/bag_02.png"
     },
     {
         id: 3,
         name: "Elite Series",
         price: 189.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/bag_03.png"
+        image: "../images/products/bag_03.png"
     },
     {
         id: 4,
         name: "Casual",
         price: 129.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/bag_04.png"
+        image: "../images/products/bag_04.png"
     },
     {
         id: 5,
         name: "Best Tote",
         price: 399.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/bag_05.png"
+        image: "../images/products/bag_05.png"
     },
     {
         id: 6,
         name: "Charming Series",
         price: 689.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "/images/products/bag_06.png"
+        image: "../images/products/bag_06.png"
     }
 ]
 
