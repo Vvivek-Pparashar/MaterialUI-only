5372ba1d56dea00c4629e11ea0e32922df9e9297
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2022-10-16 11:28:07 +0530

    home-updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
 import "./Home.css";
@@ -8,13 +8,14 @@ const Home = () => {
     <>
       <div className="home__hero">
         <div className="home__hero__img"></div>
-        <Container sx={{display:"flex", flexDirection: 'column', width : "50%" }}>
-          <Typography variant="h6">Huge Collection</Typography>
-          <Typography variant="h2">New Bags</Typography>
-          <Typography variant="p">
+        <Container sx={{display:"flex", flexDirection: 'column', width : "40%", m :0, p : 0, justifyContent:"center" }}>
+          <Typography variant="h6" sx = {{marginBottom: 1}}>Huge Collection</Typography>
+          <Typography variant="h2" sx = {{marginBottom: 4}}>New Bags</Typography>
+          <Typography variant="p" sx = {{marginBottom: 6}}  >
             Torem ipsum dolor sit amet, consectetur adipisicing elitsed do
             eiusmo tempor incididunt ut labore et dolore magna
           </Typography>
+          <Button variant="contained" sx = {{padding : "10px 0", outline: "none"}}>Shop Now</Button>
         </Container>
       </div>
     </>
